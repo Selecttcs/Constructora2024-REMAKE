a13c02f542826ab5c0dccda7166eb2a5db2a8459
Diego Cerda <diego.cerda200218@gmail.com>
2024-06-04 08:43:09 -0400

    Intento de validación para login

--- Constructora2024-REMASTERED/dist/js/scripts.js
+++ Constructora2024-REMASTERED/dist/js/scripts.js
@@ -1,7 +1,21 @@
-/*!
-* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var iniciar = document.getElementById('Iniciar');
+var nulo = "";
+function verificar(){
+    var nombre = document.getElementById('Email').value;
+    var contraseña = document.getElementById('Contraseña').value;
+    if(nombre == nulo){
+        Swal.fire({
+            icon: "info",
+            title: "Oops...",
+            text: "Es necesario rellenar los apartados para continuar.",
+        });
+    }
+    else if(contraseña == nulo){
+        Swal.fire({
+            icon: "info",
+            title: "Oops...",
+            text: "Es necesario rellenar los apartados para continuar.",
+        }); 
+    }
+
+}
